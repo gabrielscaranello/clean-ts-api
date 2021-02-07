a5eb76e6513254667071cc68a8eb66a732ea6464
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-07 10:58:47 -0300

    refactor: simplify SignUpController.handle validation code

--- src/presentation/signup.ts
+++ src/presentation/signup.ts
@@ -4,11 +4,12 @@ import { HttpRequest, HttpResponse } from './protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissignParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissignParamError('email'))
+    const requiredFields = ['name', 'email']
+
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissignParamError(field))
+      }
     }
   }
 }
