a094f9f320c27ed6acd8a13973908e3e3ae48f30
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-07 10:56:44 -0300

    refactor: move duplicated code of http response to a helper method

--- src/presentation/signup.ts
+++ src/presentation/signup.ts
@@ -1,13 +1,14 @@
 import { MissignParamError } from './errors/missign-param-error'
+import { badRequest } from './helpers/http-helper'
 import { HttpRequest, HttpResponse } from './protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new MissignParamError('name') }
+      return badRequest(new MissignParamError('name'))
     }
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new MissignParamError('email') }
+      return badRequest(new MissignParamError('email'))
     }
   }
 }
