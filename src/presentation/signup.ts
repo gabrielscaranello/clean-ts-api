aac360b4ccb1adaf450afd0ec18273bb87a9ab9d
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-02-07 10:52:19 -0300

    refactor: add customized error for missign param

--- src/presentation/signup.ts
+++ src/presentation/signup.ts
@@ -1,12 +1,13 @@
+import { MissignParamError } from './errors/missign-param-error'
 import { HttpRequest, HttpResponse } from './protocols/http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return { statusCode: 400, body: new Error('Missign param: name') }
+      return { statusCode: 400, body: new MissignParamError('name') }
     }
     if (!httpRequest.body.email) {
-      return { statusCode: 400, body: new Error('Missign param: email') }
+      return { statusCode: 400, body: new MissignParamError('email') }
     }
   }
 }
